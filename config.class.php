4bf773221014de6ebfbec79c0a3e9027445c9de2
Sergio L. <mellowonpsx@gmail.com>
2014-05-21 14:59:02 +0200

    added some class: - utils (contain utils and contains __autoload, must be required_once in each class) - db.class (a class to connect and query on database) - category.class (a class to manage category) - config.class (contain static configurazion (db and others stuff) - jquery-2.1.1.js (jquery lib, used for test) - provaajax and provalistcategory are a demo to test ajax

---
+++ config.class.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of config
+ *
+ * @author mellowonpsx
+ */
+
+require_once "utils.php";
+
+class Config
+{
+    static public $db_host = "localhost";
+    static public $db_user = "root";
+    static public $db_password = "";
+    static public $db_name = "bifora_docs";
+}
+
+?>
