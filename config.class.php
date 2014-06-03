87c8b53023a429be8d55fb201b2f3300d6ab054b
Sergio L. <mellowonpsx@gmail.com>
2014-06-03 09:48:42 +0200

    Update config.class.php

--- config.class.php
+++ config.class.php
@@ -9,7 +9,7 @@ require_once "utils.php";
 
 class Config
 {
-    static public $db_host = "localhost";
+    static public $db_host = "127.0.0.1"; //Problems with localhost name resolve
     static public $db_user = "root";
     static public $db_password = "";
     static public $db_name = "bifora_docs";
